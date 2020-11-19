f2adf8a888eea5ee6bf7274c48562b38f131a14d
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-19 22:56:04 +0900

    active hach導入

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_134210) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.integer "school_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
