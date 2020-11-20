9ae20484fa42f6ad0a5d1c2c56caf029ec555a02
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-20 11:21:50 +0900

    新規登録できるように設定&viewの変更

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,6 +1,6 @@
 class School < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '---' },
     { id: 2, name: 'テックキャンプ' },
     { id: 3, name: 'Tech Academy' },
     { id: 4, name: 'Code Camp' },
