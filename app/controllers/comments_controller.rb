6004ed0193d7a45a8a778acca9018a8635c8faa9
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-23 13:11:37 +0900

    ストロングパラメーター定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to "/apps/#{comment.app.id}"
+  end
+
+  def destroy
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content).merge(user_id: current_user.id, app_id: params[:app_id])
+    end
 end
