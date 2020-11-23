805789471e4823a578c40d78d5f4fecfabe0ee1c
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-23 19:40:19 +0900

    コメント削除機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,18 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
+    # flash[:notice] = "コメントしました"
     redirect_to "/apps/#{comment.app.id}"
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    # flash[:notice] = "コメントを削除しました"
+    redirect_to "/apps/#{comment.app.id}"
   end
 
   private
-
     def comment_params
       params.require(:comment).permit(:content).merge(user_id: current_user.id, app_id: params[:app_id])
     end
