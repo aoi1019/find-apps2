d636ef681ed9d820e26f2fdc224dfdc3e83901eb
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-23 13:05:30 +0900

    routeの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "apps#index"
-  resources :apps, except: :index
+  resources :apps, except: :index do
+    resources :comments, only: [:create, :destroy]
+  end
 end
