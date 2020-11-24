86cbaff2242d0d83af0533643220a6d557d7cee7
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-25 00:14:53 +0900

    has_one_attached :imageをuserモデルに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :school
+  has_one_attached :image
   has_many :apps, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :name, presence: true
