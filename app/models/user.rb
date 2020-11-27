72c8c00de4a76101bf88ccb1092d8e3d1d58bdcf
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-27 23:17:08 +0900

    updateの際にパスワードを入力しなくて良いメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,21 @@ class User < ApplicationRecord
   has_one_attached :image
   has_many :apps, dependent: :destroy
   has_many :comments, dependent: :destroy
-  validates :name, presence: true
-
+  validates :name, presence: true, allow_nil: true
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :school_id, numericality: { other_than: 1 }, allow_nil: true
   validates :profile, length: { maximum: 200 }
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
