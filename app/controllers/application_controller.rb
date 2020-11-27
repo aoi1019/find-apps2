53d4760fa386b0f5e7a5782cd625c831cc5616a3
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-27 23:17:24 +0900

    controllerでupdateについて定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :school_id, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :school_id])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :school_id, :image, :twitter, :qiita])
   end
 end
