ecefc626e542f829275d1ca7e4a0f563ac682904
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-29 15:56:50 +0900

    destroyアクションを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,4 +4,9 @@ class LikesController < ApplicationController
     @app = App.find(params[:id])
     like = Like.create(user_id: current_user.id, app_id: @app.id)
   end
+
+  def destroy
+    @app = App.find(paramd[:id])
+    like = Like.destroy(user_id: current_user.id, app_id: @app.id)
+  end
 end
