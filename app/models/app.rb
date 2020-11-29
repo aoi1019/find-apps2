c75da9bc64877bbb077736dadb4d2a0f825cd0b9
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-29 16:18:14 +0900

    いいねされたか判定するメソッドを追加

--- app/models/app.rb
+++ app/models/app.rb
@@ -8,4 +8,8 @@ class App < ApplicationRecord
   validates :language, presence: true
   validates :reference, presence: true
   validates :image, presence: true
+
+  def already_liked?(user)
+    self.likes.exist?(user_id: user.id)
+  end
 end
