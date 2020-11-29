1e4e7d9a8fa5a5d7d176fa40498557e8093a6bf1
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-29 21:23:02 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root "apps#index"
   resources :apps, except: :index do
     resources :comments, only: [:create, :destroy]
-    resources :likes, only: [:create, :destroy]
   end
+  post "likes/:app_id/create" => "likes#create"
+  delete "likes/:app_id/destroy" => "likes#destroy"
   resources :users, only: :show
 end
