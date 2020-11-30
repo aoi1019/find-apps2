f88fc2d3a5d1158c5517831ad2fa5634ddd08b61
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-30 12:40:13 +0900

    routeの再定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/session'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
