dae15de1990868bd04f8e3605819c13fbe4d3077
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-01 17:36:55 +0900

    create, destroyアクションを追記

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,22 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
-  def create    
+  def create
+    @app = App.find(params[:app_id])
+    @user = @app.user
+    current_user.favorite(@app)
+    respond_to do |f|
+      f.html { redirect_back(fallback_location: app_path(@app)) }
+      f.js
+    end
   end
 
   def destroy
+    @app = App.find(params[:app_id])
+    current_user.favorites.find_by(app_id: @app.id).destroy
+    respond_to do |f|
+      f.html { redirect_back(fallback_location: app_path(@app)) }
+      f.js
+    end
   end
 end
