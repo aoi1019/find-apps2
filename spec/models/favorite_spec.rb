eba3f616992f2a0e70ca6a31bf99b1b55498c23f
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-01 14:50:23 +0900

    modelのテスト実行

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @favorite = FactoryBot.build(:favorite)
+  end
+
+  it 'favoriteインスタンスが有効であることを確認' do
+    expect(@favorite).to be_valid
+  end
+
+  it 'user_idがnilの場合、無効であることを確認' do
+    @favorite.user = nil
+    expect(@favorite).not_to be_valid
+  end
+
+  it 'app_idがnilの場合、無効であることを確認' do
+    @favorite.app = nil
+    expect(@favorite).not_to be_valid
+  end
 end
