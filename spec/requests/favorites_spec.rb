c0c195c507d279207927d6388417b9f550ea6fe2
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-01 19:30:41 +0900

    [修正]requestテスト修正

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe "Favorites", type: :request do
   end
 
   context 'お気に入り登録処理' do
+    # context 'ログインしている場合' do
+    #   before do
+    #     post new_user_session_path, params: { session: { email: @user.email,password: @user.password } }
+    #   end
+    #   it '投稿にブックマーク登録ができることを確認' do
+    #     expect{
+    #       post "/favorites/#{@app.id}/create"
+    #     }.to change{Favorite.count}.by(1)
+    #   end
+    # end
+
     context 'ログインしてない場合' do
       it 'お気に入り登録はできず、ログインページへリダイレクトすることを確認' do
         expect {
