48bb829f1b04e7da422d2238c8be1c0f8a28a7e6
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-02 09:45:30 +0900

    modelテスト完了

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @notification = FactoryBot.create(:notification)
+  end
+
+  it 'インスタンスが正常なことを確認' do
+    expect(@notification).to be_valid
+  end
+
+  it 'user_idがnilの場合、無効なことを確認' do
+    @notification.user = nil
+    expect(@notification).not_to be_valid
+  end
+
+  it 'app_idがnilの場合、無効なことを確認' do
+    @notification.app_id = nil
+    expect(@notification).not_to be_valid
+  end
+
+  it 'varietyがnilの場合、無効なことを確認' do
+    @notification.variety = nil
+    expect(@notification).not_to be_valid
+  end
+  it 'from_user_idがnilの場合、無効なことを確認' do
+    @notification.from_user_id = nil
+    expect(@notification).not_to be_valid
+  end
+
 end
