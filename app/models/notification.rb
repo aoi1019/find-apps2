b63516311ddb273a6af3a426b5b3bd6810e61134
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-02 09:45:24 +0900

    バリデーション定義

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,8 @@
 class Notification < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :app_id, presence: true
+  validates :variety, presence: true
+  validates :from_user_id, presence: true
 end
