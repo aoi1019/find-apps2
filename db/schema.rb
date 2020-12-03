38fff4511b629d24f655d56e521132ad030e9dd3
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-03 12:00:01 +0900

    [修正]rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -86,8 +86,12 @@ ActiveRecord::Schema.define(version: 2020_12_03_025405) do
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -117,4 +121,5 @@ ActiveRecord::Schema.define(version: 2020_12_03_025405) do
   add_foreign_key "favorites", "users"
   add_foreign_key "likes", "apps"
   add_foreign_key "likes", "users"
+  add_foreign_key "sns_credentials", "users"
 end
