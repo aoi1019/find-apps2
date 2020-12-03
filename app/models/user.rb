a483dde04b5e882ec6dcbbae473ba122f30e8cf7
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-03 11:58:14 +0900

    OumniAuthを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :school
   has_one_attached :image
