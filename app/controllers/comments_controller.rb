ca022634582add6dfdd38981e1d649896e6a979c
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-05 23:16:08 +0900

    コメントcontrollerを編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,23 +6,23 @@ class CommentsController < ApplicationController
     @user = @app.user
     @comment = @app.comments.new(comment_params)
     if @comment.save
-      flash[:notice] = 'コメントしました'
-      redirect_to "/apps/#{@comment.app.id}"
+      flash[:notice] = 'コメントを送信しました！'
+      redirect_to app_path(@app)
       if @user != current_user
         @user.notifications.create(from_user_id: current_user.id, variety: 2, content: @comment.content, app_id: @app.id)
         @user.update_attribute(:notification, true)
       end
     else
-      flash[:alert] = 'コメントできません'
+      flash[:alert] = 'コメントは1文字以上140文字以内で入力してください！'
     end
   end
-
+  
   def destroy
     @comment = Comment.find(params[:app_id])
     @app = @comment.app
     if current_user.id == @comment.user_id
       @comment.destroy
-      flash[:notice] = 'コメントを削除しました'
+      flash[:notice] = 'コメントを削除しました！'
       redirect_to app_path(@app)
     end
   end
