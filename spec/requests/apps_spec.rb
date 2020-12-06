8a7a8bd4e90ffdcb86e6930ae7fec5c27eb3831a
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-06 11:28:01 +0900

    アプリ詳細のrequestテスト完了

--- spec/requests/apps_spec.rb
+++ spec/requests/apps_spec.rb
@@ -7,49 +7,75 @@ RSpec.describe 'Apps', type: :request do
   end
   
   describe 'トップページ' do
-    it 'indexアクションにリクエストすると正常にレスポンスが返ってくることを確認' do
+    before do
       get root_path
+    end
+    it '正常にレスポンスが返ってくることを確認' do
       expect(response.status).to eq 200
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリの名前が存在することを確認' do
-      get root_path
+    it '投稿済みのアプリの名前が存在することを確認' do
       expect(response.body).to include(@app.name)
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリの画像が存在することを確認' do
-      get root_path
+    it '投稿済みのアプリの画像が存在することを確認' do
       expect(response.body).to include "youtube.png"
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリの説明が存在することを確認' do
-      get root_path
+    it '投稿済みのアプリの説明が存在することを確認' do
       expect(response.body).to include(@app.description)
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリのプログラミング言語が存在することを確認' do
-      get root_path
+    it '投稿済みのアプリのプログラミング言語が存在することを確認' do
       expect(response.body).to include(@app.language)
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリのURLが存在することを確認' do
-      get root_path
+    it '投稿済みのアプリのURLが存在することを確認' do
       expect(response.body).to include(@app.reference)
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリの開発日数が存在することを確認' do
-      get root_path
+    it '投稿済みのアプリの開発日数が存在することを確認' do
       expect(response.body).to include("10")
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリを開発したユーザーの名前が存在することを確認' do
-      get root_path
+    it '投稿済みのアプリを開発したユーザーの名前が存在することを確認' do
       expect(response.body).to include @user.name
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのアプリを開発したユーザーの名前が存在することを確認' do
-      get root_path
+    it '投稿済みのアプリを開発したユーザーの名前が存在することを確認' do
       expect(response.body).to include "youtube.png"
     end
-    it 'indexアクションにリクエストすると検索窓が存在することを確認' do
-      get root_path
+    it '検索窓が存在することを確認' do
       expect(response.body).to include "検索"
     end
-    it 'indexアクションにリクエストするとアプリ一覧が存在することを確認' do
-      get root_path
+    it 'アプリ一覧が存在することを確認' do
       expect(response.body).to include "アプリ一覧"
+    end
+  end
+
+  describe 'アプリ詳細ページ' do
+    before do
+      get app_path(@app)
+    end
+
+    it '正常にレスポンスが返ってくることを確認' do
+      expect(response.status).to eq 200
+    end
+    it '投稿済みのアプリの名前が存在することを確認' do
+      expect(response.body).to include(@app.name)
+    end
+    it '投稿済みのアプリの画像が存在することを確認' do
+      expect(response.body).to include "youtube.png"
+    end
+    it '投稿済みのアプリの作成者が存在することを確認' do
+      expect(response.body).to include @user.name
+    end
+    it '投稿済みのアプリの説明が存在することを確認' do
+      expect(response.body).to include(@app.description)
+    end
+    it '投稿済みのアプリのプログラミング言語が存在することを確認' do
+      expect(response.body).to include(@app.language)
+    end
+    it '投稿済みのアプリのポイントが存在することを確認' do
+      expect(response.body).to include(@app.point)
+    end
+    it '投稿済みのアプリのURLが存在することを確認' do
+      expect(response.body).to include(@app.reference)
+    end
+    it '投稿済みのアプリの開発日数が存在することを確認' do
+      expect(response.body).to include("#{@app.period}")
     end
   end
 end
