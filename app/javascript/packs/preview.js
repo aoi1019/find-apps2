b47e4df482f0ef838907fd969343a00dbd7047cb
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-16 16:50:45 +0900

    プレビュー機能を削除

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,22 +1,22 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function() {
-    const ImageList = document.getElementById('image-list');
-    document.getElementById('app-image').addEventListener('change', function(e) {
+// if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+//   document.addEventListener('DOMContentLoaded', function() {
+//     const ImageList = document.getElementById('image-list');
+//     document.getElementById('app-image').addEventListener('change', function(e) {
 
-      // const imageContent = document.querySelector('img');
-      // if (imageContent) {
-      //   imageContent.remove();
-      // }
+//       const imageContent = document.querySelector('img');
+//       if (imageContent) {
+//         imageContent.remove();
+//       }
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+//       const file = e.target.files[0];
+//       const blob = window.URL.createObjectURL(file);
       
-      const imageElement = document.createElement('div');
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
+//       const imageElement = document.createElement('div');
+//       const blobImage = document.createElement('img');
+//       blobImage.setAttribute('src', blob);
       
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    });
-  });
-}
+//       imageElement.appendChild(blobImage);
+//       ImageList.appendChild(imageElement);
+//     });
+//   });
+// }
